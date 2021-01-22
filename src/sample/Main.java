92e5585e28f1dec5ca4a1a74154dadda4d322c92
David EK <47790315+engkel@users.noreply.github.com>
2021-01-22 23:43:59 +0100

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Media Player");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 1050, 600));
         primaryStage.show();
     }
 
